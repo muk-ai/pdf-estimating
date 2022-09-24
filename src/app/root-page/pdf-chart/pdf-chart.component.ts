88d0d23ce3b57fabe13df9b8202d5b4f0a7e43c4
Kenta Mukai <r@muk.ai>
2022-09-24 19:10:47 +0900

    :sparkles: pass input

--- src/app/root-page/pdf-chart/pdf-chart.component.ts
+++ src/app/root-page/pdf-chart/pdf-chart.component.ts
@@ -1,32 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Color, ScaleType } from '@swimlane/ngx-charts';
 import * as shape from 'd3-shape';
+
+interface ResultType {
+  name: string;
+  series: {
+    name: string;
+    value: number;
+  }[];
+}
 
 @Component({
   selector: 'app-pdf-chart',
   templateUrl: './pdf-chart.component.html',
   styleUrls: ['./pdf-chart.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PdfChartComponent implements OnInit {
-  results = [
-    {
-      name: 'estimation',
-      series: [
-        {
-          name: '10 hours',
-          value: 0,
-        },
-        {
-          name: '18 hours',
-          value: 100,
-        },
-        {
-          name: '30 hours',
-          value: 0,
-        },
-      ],
-    },
-  ];
+export class PdfChartComponent implements OnInit, OnChanges {
+  @Input() optimistic = 0;
+  @Input() mode = 0;
+  @Input() pessimistic = 0;
+
+  results: ResultType[] = [];
   view: [number, number] = [700, 300];
   legend = false;
   showLabels = true;
@@ -48,4 +43,26 @@ export class PdfChartComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  ngOnChanges(_changes: SimpleChanges) {
+    this.results = [
+      {
+        name: 'estimation',
+        series: [
+          {
+            name: `${this.optimistic} hours`,
+            value: 0,
+          },
+          {
+            name: `${this.mode} hours`,
+            value: 100,
+          },
+          {
+            name: `${this.pessimistic} hours`,
+            value: 0,
+          },
+        ],
+      },
+    ];
+  }
 }
