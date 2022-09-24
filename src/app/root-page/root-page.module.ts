334900fcb3041d4fab04a4df20b5021b0252c16a
Kenta Mukai <r@muk.ai>
2022-09-24 18:51:38 +0900

    :gear: move

--- src/app/root-page/root-page.module.ts
+++ src/app/root-page/root-page.module.ts
@@ -2,19 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RootPageRoutingModule } from './root-page-routing.module';
-import { RootPageComponent } from './root-page.component';
+import { RootPageComponent } from './root-page/root-page.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { PdfChartComponent } from './pdf-chart/pdf-chart.component';
 
 @NgModule({
-  declarations: [
-    RootPageComponent,
-    PdfChartComponent
-  ],
-  imports: [
-    CommonModule,
-    NgxChartsModule,
-    RootPageRoutingModule
-  ]
+  declarations: [RootPageComponent, PdfChartComponent],
+  imports: [CommonModule, NgxChartsModule, RootPageRoutingModule],
 })
-export class RootPageModule { }
+export class RootPageModule {}
