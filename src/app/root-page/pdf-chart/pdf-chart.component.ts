6d056c61ddeb87214298dae7e47200e334c89acc
Kenta Mukai <r@muk.ai>
2022-10-09 01:05:25 +0900

    :sparkles: quadratic interpolation

--- src/app/root-page/pdf-chart/pdf-chart.component.ts
+++ src/app/root-page/pdf-chart/pdf-chart.component.ts
@@ -68,32 +68,42 @@ export class PdfChartComponent implements OnInit, OnChanges {
 
   private buildSeries() {
     const data = Array(this.pessimistic + 1).fill(0);
-    const linearFunction = this.buildLinearFunction();
+    const quadraticFunction = this.buildQuadraticFunction();
     const series = data.map((_, index) => {
       return {
         name: `${index}`,
-        value: linearFunction(index),
+        value: quadraticFunction(index),
       };
     });
     return series;
   }
 
-  private buildLinearFunction() {
-    const a1 = 100 / (this.mode - this.optimistic);
-    const a2 = 100 / (this.mode - this.pessimistic);
-    const f1 = (x: number) => a1 * (x - this.optimistic);
-    const f2 = (x: number) => a2 * (x - this.pessimistic);
-    const linearFunction = (value: number) => {
+  private buildQuadraticFunction() {
+    const point1x = (this.mode + this.optimistic) / 2;
+    const point1y = 50;
+    const point2x = (this.pessimistic + this.mode) / 2;
+    const point2y = 50;
+    const a1 = point1y / (point1x - this.optimistic) ** 2;
+    const a2 = point2y / (point2x - this.mode) ** 2;
+    const f1 = (x: number) => a1 * (x - this.optimistic) ** 2;
+    const f2 = (x: number) => -a1 * (x - this.mode) ** 2 + 100;
+    const f3 = (x: number) => -a2 * (x - this.mode) ** 2 + 100;
+    const f4 = (x: number) => a2 * (x - this.pessimistic) ** 2;
+    const quadraticFunction = (value: number) => {
       if (value < this.optimistic) {
         return 0;
-      } else if (value < this.mode) {
+      } else if (value < point1x) {
         return f1(value);
-      } else if (value < this.pessimistic) {
+      } else if (value < this.mode) {
         return f2(value);
+      } else if (value < point2x) {
+        return f3(value);
+      } else if (value < this.pessimistic) {
+        return f4(value);
       } else {
         return 0;
       }
     };
-    return linearFunction;
+    return quadraticFunction;
   }
 }
