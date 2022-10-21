b30c2aab51bb91b34154f960c9799d5109737ec7
Kenta Mukai <r@muk.ai>
2022-10-21 22:30:14 +0900

    :sparkles: touch

---
+++ src/app/root-page/estimate-input-list/estimate-input-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estimate-input-list',
+  templateUrl: './estimate-input-list.component.html',
+  styleUrls: ['./estimate-input-list.component.scss'],
+})
+export class EstimateInputListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/root-page/root-page.module.ts
+++ src/app/root-page/root-page.module.ts
@@ -6,9 +6,10 @@ import { RootPageRoutingModule } from './root-page-routing.module';
 import { RootPageComponent } from './root-page/root-page.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { PdfChartComponent } from './pdf-chart/pdf-chart.component';
+import { EstimateInputListComponent } from './estimate-input-list/estimate-input-list.component';
 
 @NgModule({
-  declarations: [RootPageComponent, PdfChartComponent],
+  declarations: [RootPageComponent, PdfChartComponent, EstimateInputListComponent],
   imports: [CommonModule, NgxChartsModule, FormsModule, RootPageRoutingModule],
 })
 export class RootPageModule {}
